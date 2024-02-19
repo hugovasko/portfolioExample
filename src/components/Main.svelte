975749869688d7bf841c8b9516f5386babd863a3
Vasko Nikolov <hugovasko1@gmail.com>
2024-02-19 17:36:00 +0200

    Add formatting settings and update content in Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -45,23 +45,22 @@
 <main class="flex flex-col flex-1 p-4">
     <section
         id="introPage"
-        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+        class="flex fle-row"
     >
         <div
-            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+            class="flex flex-col lg:justify-center text-center lg:text-left gap-10 md:gap-8 lg:gap-10"
         >
-        <div>
-            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi! We're <span class="poppins text-violet-400">Code</span>Crew
-                <br />Sofware <span class="poppins text-violet-400">Developement</span>
-                Company
-            </h2>
-        </div>
-            <p class="text-base sm:text-lg md:text-xl">
-                My <span class="text-violet-400"> favorite tech</span> includes JavaScript
-                (NEXT.JS or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL
-                or Firebase/Firestore!
-            </p>
+            <div class="flex flex-col gap-5">
+                <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                    Hi! We're <span class="poppins text-violet-400">Code</span>Crew
+                    <br />Sofware <span class="poppins text-violet-400">Developement</span>
+                    Company
+                </h2>
+                <p class="text-base sm:text-lg md:text-xl">
+                    Our <span class="text-violet-400"> favorite tech</span> includes JavaScript
+                    (NEXT.JS & SvelteKit), TailwindCSS, <br/> Typescript, Java, Firebase, Node.js, Express.js, MongoDB, PostgreSQL, <br/> AWS, GCP, Azure, Docker, Kubernetes, and much more!
+                </p>
+            </div>
             <button
                 class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
             >
@@ -71,11 +70,11 @@
                 <h4 class="relative z-9">Get in touch &rarr;</h4>
             </button>
         </div>
-        <div class="relative shadow-2xl grid place-items-center">
+        <div class="relative shadow-2xl">
             <img
                 src={"images/profile.png"}
                 alt="CodeCrew logo"
-                class="object-cover z-[2] max-h-[60vh]"
+                class="object-cover z-[2] max-h-[70vh]"
             />
         </div>
         <!-- <div  class="flex p-0.5 relative max-w-[700px] w-full mx-auto">
@@ -94,13 +93,13 @@
             />
         </div> -->
     </section>
-    <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
+    <section class="py-20 flex flex-col gap-24" id="projects">
         <div class="flex flex-col gap-2 text-center">
             <h6 class="text-large sm:text-xl md:text-2xl">
-                A few of my creative endeavors.
+                A few of our creative endeavors.
             </h6>
             <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-                Curious to <span class="poppins text-violet-400">see</span> my work?
+                Curious to <span class="poppins text-violet-400">see</span> our work?
             </h3>
         </div>
         <a
